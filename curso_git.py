079f126aca16f854e12a3a0bd9a0f781f8a90827
Josue Alves <josue.alves@oceanbrasil.com>
2022-09-02 17:46:00 -0400

    realizando testes do git rebase

--- curso_git.py
+++ curso_git.py
@@ -1,3 +1,6 @@
 print("olá curso de git, fazendo uma pequena alteração\n\
 inserindo uma linha\n\
 inserindo outra linha")
+print("testando git rebase")
+print("testando git rebase de novo")
+print("testando git rebase pela ultima vez")
