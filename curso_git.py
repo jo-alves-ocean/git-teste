a820c26f0d6529964ebbe0bae0fcfaeddd5ce89e
Josue Alves <josue.alves@oceanbrasil.com>
2022-09-05 12:34:24 -0400

    voltando ao original

--- curso_git.py
+++ curso_git.py
@@ -4,8 +4,3 @@ inserindo outra linha")
 print("testando git rebase")
 print("testando git rebase de novo")
 print("testando git rebase pela ultima vez")
-<<<<<<< Updated upstream
-print("testando alteração após o stash")
-=======
-print("alteração para ser salva posteriormente")
->>>>>>> Stashed changes
